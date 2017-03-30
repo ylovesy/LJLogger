b6a5036c2eece17ad9695fc44b982378506f912d
yangyiyang <yangyiyang@lianjia.com>
2017-03-30 11:39:03 +0800

    Initial Commit

---
+++ LJLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJLogger
+//
+//  Created by yangyiyang on 2017/3/30.
+//  Copyright © 2017年 lianjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJLogger
+//
+//  Created by yangyiyang on 2017/3/30.
+//  Copyright © 2017年 lianjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
